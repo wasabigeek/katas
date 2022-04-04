59ef7695fef0b6a9719747a70cefb38e34f4aeed
wasabigeek <wasabigeek@users.noreply.github.com>
2022-04-04 11:45:00 +0800

    Third test: Start refactoring responsibilities of roll / score

--- bowling-kata/bowling_tdd.py
+++ bowling-kata/bowling_tdd.py
@@ -3,11 +3,15 @@ import unittest
 class Game:
   def __init__(self) -> None:
     self._score = 0
+    self.rolls = [None for i in range(0, 20)]
+    self.current_roll = 0
 
   def roll(self, pins):
     # tempted to remember previous roll in order to calculate score
     # roll calculates score although name does not imply that
     self._score += pins
+    self.rolls[self.current_roll] = pins
+    self.current_roll += 1
 
   def score(self):
     # does not calculate score although name implies that
@@ -30,12 +34,12 @@ class TestBowlingGame(unittest.TestCase):
     self.roll_many(20, 1)
     self.assertEqual(20, self.game.score())
 
-  def test_one_spare(self):
-    self.game.roll(5)
-    self.game.roll(5) # spare
-    self.game.roll(3)
-    self.roll_many(17, 0)
-    self.assertEqual(16, self.game.score())
+  # def test_one_spare(self):
+  #   self.game.roll(5)
+  #   self.game.roll(5) # spare
+  #   self.game.roll(3)
+  #   self.roll_many(17, 0)
+  #   self.assertEqual(16, self.game.score())
 
 if __name__ == '__main__':
     unittest.main()
