e19c731c1d3f5e3a7d4c97b450f49669ca172428
wasabigeek <wasabigeek@users.noreply.github.com>
2022-04-04 11:31:15 +0800

    First Test: Add roll method

--- bowling-kata/bowling_tdd.py
+++ bowling-kata/bowling_tdd.py
@@ -1,11 +1,14 @@
 import unittest
 
 class Game:
-  pass
+  def roll(self, pins):
+    pass
 
 class TestBowlingGame(unittest.TestCase):
   def test_gutter_game(self):
     game = Game()
+    for i in range(0, 20):
+      game.roll(0)
 
 if __name__ == '__main__':
     unittest.main()
