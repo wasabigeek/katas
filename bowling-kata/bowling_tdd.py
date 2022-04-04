0317778829b40d900c0b41ca2c9dc0c75c1dd71c
wasabigeek <wasabigeek@users.noreply.github.com>
2022-04-04 11:36:20 +0800

    SecondTest: refactor test setup

--- bowling-kata/bowling_tdd.py
+++ bowling-kata/bowling_tdd.py
@@ -11,17 +11,19 @@ class Game:
     return self._score
 
 class TestBowlingGame(unittest.TestCase):
+  def setUp(self):
+    self.game = Game()
+    return super().setUp()
+
   def test_gutter_game(self):
-    game = Game()
     for i in range(0, 20):
-      game.roll(0)
-    self.assertEqual(0, game.score())
+      self.game.roll(0)
+    self.assertEqual(0, self.game.score())
 
   def test_all_ones(self):
-    game = Game()
     for i in range(0, 20):
-      game.roll(1)
-    self.assertEqual(20, game.score())
+      self.game.roll(1)
+    self.assertEqual(20, self.game.score())
 
 if __name__ == '__main__':
     unittest.main()
