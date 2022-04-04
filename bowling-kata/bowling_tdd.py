524e6542782e21d402584ec82cf88fe6005a3e15
wasabigeek <wasabigeek@users.noreply.github.com>
2022-04-04 11:32:20 +0800

    First Test: Add score for gutter game

--- bowling-kata/bowling_tdd.py
+++ bowling-kata/bowling_tdd.py
@@ -4,11 +4,15 @@ class Game:
   def roll(self, pins):
     pass
 
+  def score(self):
+    return 0
+
 class TestBowlingGame(unittest.TestCase):
   def test_gutter_game(self):
     game = Game()
     for i in range(0, 20):
       game.roll(0)
+    self.assertEqual(0, game.score())
 
 if __name__ == '__main__':
     unittest.main()
