59837480d4a56672ae67177dc680bd4703e51915
wasabigeek <wasabigeek@users.noreply.github.com>
2022-04-09 23:26:30 +0800

    Try refactoring out _get_roll_score

--- bowling-kata/1_running_score/bowling_tdd.py
+++ bowling-kata/1_running_score/bowling_tdd.py
@@ -14,13 +14,13 @@ class Game:
     frame_index = 0
     for frame in range(0, 10):
       if self._is_strike(frame_index):
-        score += 10 + self.rolls[frame_index + 1] + self.rolls[frame_index + 2]
+        score += 10 + self._get_roll_score(frame_index + 1) + self._get_roll_score(frame_index + 2)
         frame_index += 1
       elif self._is_spare(frame_index):
-        score += 10 + self.rolls[frame_index + 2]
+        score += 10 + self._get_roll_score(frame_index + 2)
         frame_index += 2
       else:
-        score += self.rolls[frame_index] + self.rolls[frame_index + 1]
+        score += self._get_roll_score(frame_index) + self._get_roll_score(frame_index + 1)
         frame_index += 2
 
     return score
@@ -35,12 +35,17 @@ class Game:
 
     return data
 
-
   def _is_spare(self, frame_index):
-    return (self.rolls[frame_index] + self.rolls[frame_index + 1]) == 10
+    return (self._get_roll_score(frame_index) + self._get_roll_score(frame_index + 1)) == 10
 
   def _is_strike(self, frame_index):
-    return self.rolls[frame_index] == 10
+    return self._get_roll_score(frame_index) == 10
+
+  # trying my best to avoid adding new objects as an experiment
+  def _get_roll_score(self, index):
+    if self.rolls[index] is None:
+      return 0
+    return self.rolls[index]
 
 class TestBowlingGame(unittest.TestCase):
   def setUp(self):
@@ -83,6 +88,9 @@ class TestBowlingGame(unittest.TestCase):
     self.roll_many(12, 10)
     self.assertEqual(300, self.game.score())
 
+  def test_empty_game(self):
+    self.assertEqual(0, self.game.score())
+
   # def test_spare_in_last_frame(self):
   #   self.roll_many(0, 18)
   #   self.roll_spare()
@@ -109,6 +117,24 @@ class TestBowlingGameScorecard(unittest.TestCase):
     self.assertEqual(
       [
         { "rolls": [], "score": None },
+        { "rolls": [], "score": None },
+        { "rolls": [], "score": None },
+        { "rolls": [], "score": None },
+        { "rolls": [], "score": None },
+        { "rolls": [], "score": None },
+        { "rolls": [], "score": None },
+        { "rolls": [], "score": None },
+        { "rolls": [], "score": None },
+        { "rolls": [], "score": None },
+      ],
+      self.game.frames_data()
+    )
+
+  def test_partial_frame(self):
+    self.roll_many(1, 3)
+    self.assertEqual(
+      [
+        { "rolls": [3], "score": 3 },
         { "rolls": [], "score": None },
         { "rolls": [], "score": None },
         { "rolls": [], "score": None },
