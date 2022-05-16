512a10946783f8efd1bf9ee5ccd86deaa0790301
wasabigeek <wasabigeek@users.noreply.github.com>
2022-05-16 21:05:29 +0800

    Strip left whitespace

--- word-wrap/word_wrapper.rb
+++ word-wrap/word_wrapper.rb
@@ -9,12 +9,12 @@ def word_wrap(string, col_num)
     wrapped += char
     counter += 1
     if counter == col_num
-      lines << wrapped
+      lines << wrapped.lstrip
       wrapped = ''
       counter = 0
     end
   end
-  lines << wrapped if wrapped != ''
+  lines << wrapped.lstrip if wrapped != ''
 
   lines.join("\n")
 end
@@ -29,6 +29,12 @@ class WordWrapTest < Minitest::Test
   def test_splits_string_exceeding_col_num
     string = 'lorem ipsum dolor sit amet'
     wrapped = word_wrap(string, 21)
-    assert_equal("lorem ipsum dolor sit\n amet", wrapped)
+    assert_equal("lorem ipsum dolor sit\namet", wrapped)
   end
+
+  # def test_splits_do_not_break_small_words
+  #   string = 'lorem ipsum dolor sit amet'
+  #   wrapped = word_wrap(string, 20)
+  #   assert_equal("lorem ipsum dolor\n sit amet", wrapped)
+  # end
 end
