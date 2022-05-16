eae25599c4ec49fd2badf2f333ea54cf5050c887
wasabigeek <wasabigeek@users.noreply.github.com>
2022-05-16 21:00:29 +0800

    Naively break lines

--- word-wrap/word_wrapper.rb
+++ word-wrap/word_wrapper.rb
@@ -1,7 +1,17 @@
 require 'minitest/autorun'
 
 def word_wrap(string, col_num)
-  string
+  counter = 0
+  wrapped = ''
+  string.each_char do |char|
+    wrapped += char
+    counter += 1
+    if counter == col_num
+      wrapped += "\n"
+      counter = 0
+    end
+  end
+  wrapped
 end
 
 class WordWrapTest < Minitest::Test
@@ -9,5 +19,11 @@ class WordWrapTest < Minitest::Test
     string = 'lorem ipsum'
     wrapped = word_wrap(string, 20)
     assert_equal(string, wrapped)
+  end
+
+  def test_splits_string_exceeding_col_num
+    string = 'lorem ipsum dolor sit amet'
+    wrapped = word_wrap(string, 21)
+    assert_equal("lorem ipsum dolor sit\n amet", wrapped)
   end
 end
