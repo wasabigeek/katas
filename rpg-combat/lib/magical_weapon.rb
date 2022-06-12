ed50f56683a80626aa72e1775d85a1cce5c519d9
wasabigeek <wasabigeek@users.noreply.github.com>
2022-06-12 17:21:33 +0800

    Add more functionality to MagicalWeapon

--- rpg-combat/lib/magical_weapon.rb
+++ rpg-combat/lib/magical_weapon.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
 class MagicalWeapon
-  def initialize(damage:)
+  attr_reader :health
+
+  def initialize(health:, damage:)
+    @health = health
     @damage = damage
   end
 
   def call(target:, **)
+    return if destroyed?
+
     target.receive_damage(@damage)
+    @health -= 1
+  end
+
+  def destroyed?
+    @health <= 0
   end
 end
--- rpg-combat/tests/magical_weapon_test.rb
+++ rpg-combat/tests/magical_weapon_test.rb
@@ -6,11 +6,34 @@ require 'magical_weapon'
 
 class MagicalWeaponTest < Minitest::Test
   def test_call_triggers_damage_on_target
-    object = MagicalWeapon.new(damage: 200)
+    object = MagicalWeapon.new(health: 10, damage: 200)
     character_mock = Minitest::Mock.new
-    character2_mock = Minitest::Mock.new
-    character2_mock.expect(:receive_damage, true, [200])
-    object.call(user: character_mock, target: character2_mock)
-    character2_mock.verify
+    character_mock.expect(:receive_damage, true, [200])
+    object.call(user: nil, target: character_mock)
+    character_mock.verify
+  end
+
+  def test_call_causes_health_loss
+    object = MagicalWeapon.new(health: 10, damage: 200)
+    character_mock = Minitest::Mock.new
+    character_mock.expect(:receive_damage, true, [200])
+    object.call(user: nil, target: character_mock)
+
+    assert_equal 9, object.health
+  end
+
+  def test_destroyed_at_zero_health
+    object = MagicalWeapon.new(health: 1, damage: 1)
+    refute object.destroyed?
+
+    object2 = MagicalWeapon.new(health: 0, damage: 1)
+    assert object2.destroyed?
+  end
+
+  def test_call_does_nothing_when_object_is_destroyed
+    object = MagicalWeapon.new(health: 0, damage: 1)
+    character_mock = Minitest::Mock.new
+    object.call(user: nil, target: character_mock)
+    character_mock.verify
   end
 end
