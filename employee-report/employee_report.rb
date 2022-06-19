afd4bc1b0dd5eb80d4514d2464959dba2b3da962
wasabigeek <wasabigeek@users.noreply.github.com>
2022-06-19 15:11:29 +0800

    Get first requirement passing

--- employee-report/employee_report.rb
+++ employee-report/employee_report.rb
@@ -1,2 +1,13 @@
 class EmployeeReport
+  def initialize(employees)
+    @employees = employees
+  end
+
+  def sunday_allowed_employees
+    employees.select { |employee| employee[:age] >= 18 }
+  end
+
+  private
+
+  attr_reader :employees
 end
