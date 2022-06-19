a1d28085423d926c5fd82af6d9f6c657bbbfaa69
wasabigeek <wasabigeek@users.noreply.github.com>
2022-06-19 15:10:29 +0800

    Add test for first requirement

--- employee-report/employee_report_test.rb
+++ employee-report/employee_report_test.rb
@@ -2,7 +2,15 @@ require "minitest/autorun"
 require_relative "./employee_report"
 
 class EmployeeReportTest < Minitest::Test
-  def test_base
-    assert true
+  def test_return_employees_older_than_18
+    employees = [
+      { name: 'Max', age: 17 },
+      { name: 'Sepp', age: 18 },
+      { name: 'Nina', age: 15 },
+      { name: 'Mike', age: 51 },
+    ]
+    result = EmployeeReport.new(employees).sunday_allowed_employees
+
+    assert_equal ['Sepp', 'Mike'], result.map { |employee| employee[:name] }
   end
 end
