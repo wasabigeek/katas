57f95ef0c39687db5fd10cc001b84c17a5b540b6
wasabigeek <wasabigeek@users.noreply.github.com>
2022-06-20 17:51:49 +0800

    Make test data more specific

--- employee-report/employee_report_test.rb
+++ employee-report/employee_report_test.rb
@@ -6,30 +6,20 @@ class EmployeeReportTest < Minitest::Test
     employees = [
       { name: 'Max', age: 17 },
       { name: 'Sepp', age: 18 },
-      { name: 'Nina', age: 15 },
-      { name: 'Mike', age: 51 },
+      { name: 'Mike', age: 51 }
     ]
     result = EmployeeReport.new(employees).sunday_allowed_employees
 
-    # assert_equal(
-    #   ['Sepp', 'Mike'].sort,
-    #   result.map { |employee| employee[:name] }.sort
-    # )
-
-    # this is less concise, but is closer to the intent
     result_names = result.map { |employee| employee[:name] }
     assert_includes result_names, 'Sepp'
     assert_includes result_names, 'Mike'
     refute_includes result_names, 'Max'
-    refute_includes result_names, 'Nina'
   end
 
   def test_result_sorted_by_name
     employees = [
-      { name: 'Max', age: 17 },
       { name: 'Sepp', age: 18 },
-      { name: 'Nina', age: 15 },
-      { name: 'Mike', age: 51 },
+      { name: 'Mike', age: 51 }
     ]
     result = EmployeeReport.new(employees).sunday_allowed_employees
 
