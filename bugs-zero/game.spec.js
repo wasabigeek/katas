6d4b45467c1df982204a3b65c1263d8582eb0273
wasabigeek <wasabigeek@users.noreply.github.com>
2022-07-22 22:35:21 +0800

    Start on tests for add method

--- bugs-zero/game.spec.js
+++ bugs-zero/game.spec.js
@@ -10,6 +10,21 @@ describe("The test environment", function() {
   });
 });
 
-describe("Your specs...", function() {
-  // it ...
+describe("add", function() {
+  it("correctly increases number of players", () => {
+    const game = new Game();
+    expect(game.howManyPlayers()).toEqual(0);
+    game.add("bob");
+    expect(game.howManyPlayers()).toEqual(1);
+  });
+
+  it("does not allow >6 players", () => {
+    const game = new Game();
+    for (var i = 0; i < 6; i++) {
+      game.add(`bob${i}`);
+    }
+    expect(game.howManyPlayers()).toEqual(6);
+    game.add("alice");
+    expect(game.howManyPlayers()).toEqual(6);
+  });
 });
