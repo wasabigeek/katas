98fe8632332495fecf53494504bd4264fe428297
wasabigeek <wasabigeek@users.noreply.github.com>
2022-07-24 11:52:11 +0800

    Add tests for player not in penalty

--- bugs-zero/game.spec.js
+++ bugs-zero/game.spec.js
@@ -85,18 +85,33 @@ describe("roll", function() {
       expect(game.currentPlayerState().player.place).toEqual(0);
     });
   });
-  // describe("when player is not in penalty box", () => {
-  //   it("cannot get out when rolling an even number", () => {
-  //     const game = new Game();
-  //     game.add("bob");
-  //     game.wrongAnswer();
-  //     expect(game.currentPlayerState().player.inPenaltyBox).toEqual(true);
+  describe("when player is not in penalty box", () => {
+    var game;
+    beforeEach(() => {
+      game = new Game();
+      game.add("bob");
+      expect(game.currentPlayerState().player.inPenaltyBox).toEqual(false);
+    });
 
-  //     game.roll(2);
-  //     expect(game.currentPlayerState().player.inPenaltyBox).toEqual(true);
-  //     expect(game.currentPlayerState().isGettingOutOfPenaltyBox).toEqual(false);
-  //   });
-  // });
+    it("askQuestion regardless of even or odd number", () => {
+      game.roll(1);
+      expect(game.questionBank().totalRemaining).toEqual(199);
+      game.roll(2);
+      expect(game.questionBank().totalRemaining).toEqual(198);
+    });
+
+    it("increments player place by the rolled number", () => {
+      game.roll(1);
+      game.roll(2);
+      expect(game.currentPlayerState().player.place).toEqual(3);
+    });
+
+    it("normalises player place within 0 to 11", () => {
+      game.roll(11);
+      game.roll(1);
+      expect(game.currentPlayerState().player.place).toEqual(0);
+    });
+  });
 });
 
 describe("wasCorrectlyAnswered", () => {
