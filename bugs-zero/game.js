34e0a79603e92a9710fa05af99618aad4c0dacbc
wasabigeek <wasabigeek@users.noreply.github.com>
2022-07-30 13:37:12 +0800

    Inline createRockQuestion

--- bugs-zero/game.js
+++ bugs-zero/game.js
@@ -89,15 +89,11 @@ exports.Game = function(props) {
     return 'Rock';
   };
 
-  this.createRockQuestion = function(index){
-    return "Rock Question "+index;
-  };
-
   for(var i = 0; i < 50; i++){
     popQuestions.push("Pop Question "+i);
     scienceQuestions.push("Science Question "+i);
     sportsQuestions.push("Sports Question "+i);
-    rockQuestions.push(this.createRockQuestion(i));
+    rockQuestions.push("Rock Question "+i);
   };
 
   this.isPlayable = function(howManyPlayers){
