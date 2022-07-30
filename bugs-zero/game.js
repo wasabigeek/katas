36e32c6ebd30b54a38801cb834a9d26b029e8676
wasabigeek <wasabigeek@users.noreply.github.com>
2022-07-30 14:51:25 +0800

    Extract Places to it's own file

--- bugs-zero/game.js
+++ bugs-zero/game.js
@@ -1,3 +1,5 @@
+const { Places } = require("./Places");
+
 exports = typeof window !== "undefined" && window !== null ? window : global;
 
 class Player {
@@ -6,31 +8,6 @@ class Player {
     this.purse = purse;
     this.inPenaltyBox = inPenaltyBox;
     this.name = name;
-  }
-}
-
-class Places {
-  static total = 12;
-
-  // encapsulate mapping of place to question category
-  static questionCategory = ({ playerPlace }) => {
-    if([0, 4, 8].includes(playerPlace))
-      return 'Pop';
-    if([1, 5, 9].includes(playerPlace))
-      return 'Science';
-    if([2, 6, 10].includes(playerPlace))
-      return 'Sports';
-
-    return 'Rock';
-  }
-  // encapsulate how many places there are here
-  static indexFromRoll = ({ roll, playerPlace }) => {
-    var newPlace = playerPlace + roll;
-    if(newPlace > this.total - 1){
-      newPlace = newPlace - this.total;
-    }
-
-    return newPlace;
   }
 }
 
---
+++ bugs-zero/spec/Places.spec.js
@@ -0,0 +1,6 @@
+require('../Places.js');
+
+describe("questionCategory", function() {
+  it("returns Pop on the right indexes", function() {
+  });
+});
